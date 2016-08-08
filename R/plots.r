406b078ee54440ec3606326292cc849fa96fa977
Isa Cobo <icobo@carrerasresearch.org>
2016-08-08 11:49:30 +0200

    Rename a variable

--- R/plots.r
+++ R/plots.r
@@ -41,18 +41,18 @@ screeplot.SVDAnalysis = function(result) {
 #' @importFrom ggplot2 ggplot aes_string geom_tile scale_fill_manual
 #' scale_color_manual xlab theme_bw theme geom_vline
 plot_significance = function(result) {
-  colors = c(rev(brewer.pal(nlevels(result$significance$Sig) - 1, 'Oranges')), 'transparent')
+  colors_palette = c(rev(brewer.pal(nlevels(result$significance$Sig) - 1, 'Oranges')), 'transparent')
   result = ggplot(result$significance, aes_string(x = 'PC', y = 'Variable')) +
     geom_vline(xintercept = result$limited_significant_PC, color = 'grey') +
     scale_x_discrete() +
     geom_tile(aes_string(fill = 'Sig', color = 'Sig'), width = 0.6,
               height = 0.6) +
     scale_fill_manual(
-      values = colors,
+      values = colors_palette,
       drop = FALSE,
       name = 'P-Value') +
     scale_color_manual(
-      values = colors,
+      values = colors_palette,
       drop = FALSE,
       name = 'P-Value') +
     xlab('Component') +
