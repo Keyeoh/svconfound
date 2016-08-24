cf235a302cf3ca806f3754910f55501b7201ac42
Isa Cobo <icobo@carrerasresearch.org>
2016-08-24 11:35:00 +0200

    Improve performance on calculating control probes

--- R/utils.r
+++ R/utils.r
@@ -82,13 +82,6 @@ get_control_variables = function(rgSet) {
     ext = getControlAddress(rgSet, controlType = c('EXTENSION'))
     tr = getControlAddress(rgSet, controlType = c('TARGET REMOVAL'))
     hyb = getControlAddress(rgSet, controlType = c('HYBRIDIZATION'))
-    CPP = rbind(getGreen(rgSet)[bc1[1:3], ],
-                getRed(rgSet)[bc1[7:9], ],
-                getRed(rgSet)[bc2[1:4], ],
-                getGreen(rgSet)[tr[1:2], ],
-                getGreen(rgSet)[hyb[1:3], ],
-                getRed(rgSet)[ext[1:2], ],
-                getGreen(rgSet)[ext[3:4], ])
     controlNames = c('BSC-I C1 Grn', 'BSC-I C2 Grn', 'BSC-I C3 Grn',
                      'BSC-I C4 Red', 'BSC-I C5 Red', 'BSC-I C6 Red',
                      'BSC-II C1 Red', 'BSC-II C2 Red', 'BSC-II C3 Red',
@@ -96,11 +89,15 @@ get_control_variables = function(rgSet) {
                      'Hyb (Low) Grn', 'Hyb (Medium) Grn', 'Hyb (High) Grn',
                      'Extension (A) Red', 'Extension (T) Red', 'Extension (C) Grn',
                      'Extension (G) Grn')
-    rownames(CPP) = controlNames
-    colnames(CPP) = colnames(rgSet)
+    CPP = rbind(getGreen(rgSet)[bc1[1:3], ],
+                getRed(rgSet)[bc1[7:9], ],
+                getRed(rgSet)[bc2[1:4], ],
+                getGreen(rgSet)[tr[1:2], ],
+                getGreen(rgSet)[hyb[1:3], ],
+                getRed(rgSet)[ext[1:2], ],
+                getGreen(rgSet)[ext[3:4], ])
+    dimnames(CPP) = list(controlNames, colnames(rgSet))
     dataC2.m = t(log2(CPP))
-    rownames(dataC2.m) = colnames(rgSet)
-    colnames(dataC2.m) = controlNames
   }
   return(dataC2.m)
 }
